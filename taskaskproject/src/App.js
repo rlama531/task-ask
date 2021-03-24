66dab839d42e235726b9b4dee9b513a026779200
rlama531 <roberto.lama18@gmail.com>
2021-03-24 19:12:11 -0400

    Created React App and FrontEnd header complete

---
+++ taskaskproject/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Header from "./Header.js";
+/* import FilterBar from "./filterBar.js" */ 
+/* import Posts from "./posts.js" */ 
+
+function App() {
+  return (
+    <div className="app">
+      <Header/>
+      {/* FilterBar */}
+      {/* Posts */}
+    </div>
+  );
+}
+
+export default App;
---
+++ taskaskproject/src/Header.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import "./Header.css"; 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
+import MessageIcon from '@material-ui/icons/Message';
+import InputBase from '@material-ui/core/InputBase'; 
+
+
+const searchBarTheme = createMuiTheme({
+    props : { 
+        MuiInputBase: { 
+            placeholder: "Search Task For Your Next Task!",
+            autoComplete: "hello",
+        },
+    },
+
+
+});
+          
+
+function Header() {
+    return (
+        <div className="header">
+            <IconButton > 
+                <div className="logo">
+                    Tasks4Cash $$$
+                </div>
+                {/* create logo soon */}
+            </IconButton>
+            <ThemeProvider theme={searchBarTheme}> 
+                <InputBase className="searchBar" > 
+                    {/* this needs to be changed to have functionallity; 
+                        potentiall we can get rid of theme provider and 
+                        using inputbase and move to a html searchbar */}
+                </InputBase> 
+            </ThemeProvider>
+            
+            <div className="menu"> 
+                <IconButton> 
+                    <MessageIcon fontSize="large" className="messagesIcon"/>
+                </IconButton>
+                <IconButton> 
+                    <PersonIcon fontSize="large" className="personIcon"/> 
+                </IconButton>
+            
+            </div> 
+
+
+        </div>
+    )
+}
+
+export default Header
