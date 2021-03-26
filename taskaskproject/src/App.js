415a1ea71e5f7f28a0b63011eb5cd384335537c5
rlama531 <roberto.lama18@gmail.com>
2021-03-25 20:42:16 -0400

    Added new component

--- taskaskproject/src/App.js
+++ taskaskproject/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import './App.css';
 import Header from "./Header.js";
 import Posts from "./Posts.js";
+import CreatePosts from "./CreatePosts.js"; 
 
 function App() {
   return (
     <div className="app">
       <Header/>
+      {/* <CreatePosts/>  */}
       <Posts/> 
     </div>
   );
---
+++ taskaskproject/src/CreatePosts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./CreatePosts.css";
+
+function CreatePosts() {
+    return (
+        <div className="createPostsButton">
+            
+        </div>
+    )
+}
+
+export default CreatePosts
